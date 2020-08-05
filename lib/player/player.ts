16fbaee7fe5bef8cb057a2e1f22eb71c047f3339
youkaisteve <youkaisteve@gmail.com>
2020-08-05 15:03:04 +0800

    feat(player): after call methods, handle context to customer

--- lib/player/player.ts
+++ lib/player/player.ts
@@ -24,14 +24,23 @@ export default class Player {
 
     /**
      * 开始
+     * @param blocker 需要异步启动的业务，可设置blocker，待blocker返回true之后，才会真正启动，否则会停止
      */
-    start() {
+    async start(blocker: () => Promise<boolean>) {
         if (this.context.status !== 0) {
             console.warn('player has already started');
             return;
         }
         this.context.status = 1;
-        this.play();
+        this.playerOption.handler(this.context);
+
+        const blockResult = await blocker();
+        if (blockResult) {
+            this.play();
+        } else {
+            // blocker返回失败结果，状态重置为0
+            this.stop();
+        }
     }
     /**
      * 暂停
@@ -43,6 +52,7 @@ export default class Player {
         }
         clearInterval(this.timer);
         this.context.status = 2;
+        this.handleContent();
     }
     /**
      * 恢复，暂停后使用
@@ -62,6 +72,7 @@ export default class Player {
         clearInterval(this.timer);
         this.context.status = 0;
         this.context.current = 0;
+        this.handleContent();
     }
     /**
      * 加速
@@ -73,6 +84,7 @@ export default class Player {
         }
         this.speedIndex++;
         this.context.speed = this.context.speedList[this.speedIndex];
+        this.handleContent();
     }
     /**
      * 减速，最低减到一倍速，不能慢放
@@ -84,19 +96,23 @@ export default class Player {
         }
         this.speedIndex--;
         this.context.speed = this.context.speedList[this.speedIndex];
+        this.handleContent();
     }
     /**
      * 播放
      */
     private play() {
         clearInterval(this.timer);
-        this.playerOption.handler(this.context);
         this.timer = setInterval(() => {
             if (this.context.current === this.context.total) {
                 this.stop();
             }
             this.context.current++;
-            this.playerOption.handler(this.context);
+            this.handleContent();
         }, this.context.speed * this.context.interval * 1000);
+    }
+
+    private handleContent() {
+        this.playerOption.handler(this.context);
     }
 }
